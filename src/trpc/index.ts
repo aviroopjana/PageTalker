0dea870242836c43cd9893e4b846cdbc93868ece
Aviroop Jana <aviroopjana@gmail.com>
2023-10-07 12:30:59 +0530

    chore: Configure Prisma ORM and connect to Supabase ✅

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,20 @@
-import { router } from './trpc';
+import { useUser } from '@clerk/nextjs';
+import { publicProcedure, router } from './trpc';
+import { TRPCError } from '@trpc/server';
 
 export const appRouter = router({
-  // ...
+  // test: publicProcedure.query(()=> {
+  //   return new Response()
+  // })
+  authCallback: publicProcedure.query(()=> {
+    const { user } = useUser();
+
+    if(!user?.id || !user?.username) throw new TRPCError({"code": "UNAUTHORIZED"})
+
+    //check if the user is in database
+    
+    return {success: true};
+  })
 });
 
 // Export type router type signature,
